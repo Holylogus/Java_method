6cbbf9aaee248540ca54a39c43a54bcc630f4fcf
Holylogus <hamar.patrik94@gmail.com>
2023-02-23 17:39:16 +0100

    1.feladat jav

--- metodusok/src/metodusok/Metodusok.java
+++ metodusok/src/metodusok/Metodusok.java
@@ -1,20 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package metodusok;
 
-/**
- *
- * @author kript
- */
+
 public class Metodusok {
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+       
     }
-    
-}
+    private static void elso10SzamOsszege(){
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += i;
+        }
+       return osszeg;
+    }
+    private static void kiir(int osszeg){
+        System.out.println(osszeg);
+    }
+}   
