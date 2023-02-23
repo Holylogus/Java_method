9697272a12b88c0c4dac72480b9abe4433acb7b3
Holylogus <hamar.patrik94@gmail.com>
2023-02-23 19:01:59 +0100

    metódusba szervezés

--- metodusok/src/metodusok/Metodusok.java
+++ metodusok/src/metodusok/Metodusok.java
@@ -9,20 +9,31 @@ public class Metodusok {
         kiir("Elsõ 10 szám összege:" + elso10SzamOsszege());
         kiir("Két szám összege:" + osszead(7, 4));
         int szam1 = 7, szam2 = 4;
+        int szam3 = 3;
+        int szam4 = 2;
         int osszeg = osszead(szam1, szam2);
         kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg));
         //írjunk ki 4 szám összegét
-        int negySzamOsszeg = osszead(szam1, szam2)+osszead(szam1, szam2);
-        kiir("4 Szám összege:"+ negySzamOsszeg);
-        int szam3 = 3;
-        int szam4 = 2;
-        osszeg = osszead(szam3, szam4);
-        osszeg += osszead(osszeg, szam2);
-        double gyok = Math.sqrt(osszeg);
-        kiir("%d+%d+%d gyoke = %4f\n".formatted(szam3,szam4,szam2,gyok));
+      
+        kiir("4 Szám összege:"+ negySzamosszege(szam1, szam2, szam3, szam4));
+        
+        kiir("%d+%d+%d gyoke = %4f\n".formatted(szam3,szam4,szam2,gyokvonas(szam1, szam3, szam4)));
+        
         
         
  
+    }
+    private static double gyokvonas(int a, int b, int c){
+        int osszeg = osszead(a, b);
+        osszeg += osszead(osszeg, c);
+        double gyok = Math.sqrt(osszeg);
+        return gyok;
+            
+    }
+    private static int negySzamosszege(int a, int b, int c, int d){
+        int osszeg = osszead(a, b);
+        osszeg += osszead(c, d);
+        return osszeg;
     }
     private static int elso10SzamOsszege(){
         int osszeg = 0;
