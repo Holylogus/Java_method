a7e87010607ab2acac6ddf065b6a75fba1ed94c1
Holylogus <hamar.patrik94@gmail.com>
2023-02-23 17:55:37 +0100

    2. feladat kiírások

--- metodusok/src/metodusok/Metodusok.java
+++ metodusok/src/metodusok/Metodusok.java
@@ -6,16 +6,20 @@ public class Metodusok {
 
  
     public static void main(String[] args) {
-       
+        kiir("Első 10 szám összege:" + elso10SzamOsszege());
+        kiir("Két szám összege:" + osszead(5, 2));
     }
-    private static void elso10SzamOsszege(){
+    private static int elso10SzamOsszege(){
         int osszeg = 0;
         for (int i = 0; i < 10; i++) {
             osszeg += i;
         }
        return osszeg;
     }
-    private static void kiir(int osszeg){
-        System.out.println(osszeg);
+    private static int osszead(int a, int b){
+        return a+b;
+    }
+    private static void kiir(String szoveg){
+        System.out.println(szoveg);
     }
 }   
