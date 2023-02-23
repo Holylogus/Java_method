10be2aadcdca4880c7cf59239b514489d4865559
Holylogus <hamar.patrik94@gmail.com>
2023-02-23 18:17:03 +0100

    4 szám összege

--- metodusok/src/metodusok/Metodusok.java
+++ metodusok/src/metodusok/Metodusok.java
@@ -7,10 +7,13 @@ public class Metodusok {
  
     public static void main(String[] args) {
         kiir("Elsõ 10 szám összege:" + elso10SzamOsszege());
-        kiir("Két szám összege:" + osszead(5, 2));
+        kiir("Két szám összege:" + osszead(7, 4));
         int szam1 = 7, szam2 = 4;
         int osszeg = osszead(szam1, szam2);
         kiir("%d + %d = %d\n".formatted(szam1, szam2, osszeg));
+        //írjunk ki 4 szám összegét
+        int negySzamOsszeg = osszead(szam1, szam2)+osszead(szam1, szam2);
+        kiir("4 Szám összege:"+negySzamOsszeg);
     }
     private static int elso10SzamOsszege(){
         int osszeg = 0;
